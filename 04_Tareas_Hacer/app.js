29de63aafab80c2fb4f1e9813becafe15ec0fa8e
baubyte <paredbaez.martin@gmail.com>
2022-01-18 18:12:50 -0300

    menu basico

--- 04_Tareas_Hacer/app.js
+++ 04_Tareas_Hacer/app.js
@@ -1,1 +1,15 @@
 require('colors');
+const {mostrarMenu, pausa} = require('./helpers/messages');
+//Punto de Inicio
+const main = async ()=>{
+    let option = '';
+    do {
+        option = await mostrarMenu();
+        //Solo mostrar la Pausa cuando no sea 0
+        if (option !=='0') {
+            await pausa();
+        }
+    } while (option !=='0');
+}
+
+main();
