61014587910892256800b3b16c7088e8df02bc2e
baubyte <paredbaez.martin@gmail.com>
2022-01-19 06:05:58 -0300

    opcion crear tarea

--- 04_Tareas_Hacer/app.js
+++ 04_Tareas_Hacer/app.js
@@ -1,14 +1,50 @@
 require('colors');
 
-const {inquirerMenu, pausa} = require('./helpers/inquirer');
-const Tarea = require('./models/tarea');
+const {
+    inquirerMenu,
+    pausa,
+    leerInput
+} = require('./helpers/inquirer');
+const Tareas = require('./models/tareas');
 //Punto de Inicio
-const main = async ()=>{
+const main = async () => {
+    //Capturar las opciones
     let option = '';
+    //Instancia del modelo de las tareas
+    const tareas = new Tareas();
     do {
         option = await inquirerMenu();
+
+        switch (option) {
+            case '1':
+                //Input para ingresar la descripción
+                const descripcion = await leerInput('Ingrese Descripción:');
+                //Creamos la tarea
+                tareas.createTarea(descripcion);
+                break;
+            case '2':
+                //
+                console.log(tareas._listado);
+                break;
+            case '3':
+
+                break;
+            case '4':
+
+                break;
+            case 5:
+
+                break;
+            case '6':
+
+                break;
+            case '0':
+
+                break;
+
+        }
         await pausa();
-    } while (option !=='0');
+    } while (option !== '0');
 }
 
 main();
