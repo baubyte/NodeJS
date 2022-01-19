80f1e407e29d90e9cf791b7b621d332c126d3c0e
baubyte <paredbaez.martin@gmail.com>
2022-01-19 06:59:49 -0300

    listar datos

--- 04_Tareas_Hacer/helpers/manageDb.js
+++ 04_Tareas_Hacer/helpers/manageDb.js
@@ -1,12 +1,27 @@
 const fs = require('fs');
+//Directorio donde se va a guardar el archivo
+const  file = './dB/data.json';
 
 /**
  * Guarda la información en un archivo json
  * @param {*} data 
  */
 const saveDb = (data) => {
-    //Directorio donde se va a guardar el archivo
-    const  file = './dB/data.json';
     //Guardamos el archivo
     fs.writeFileSync(file, JSON.stringify(data), 'utf8');
+}
+
+const readDb = () => {
+    //Verifica el archivo
+    if (!fs.existsSync(file)) {
+        return null;
+    }
+    //Leemos el archivo
+    const data = fs.readFileSync(file, {encoding: 'utf8'});
+    //retorna la información
+    return JSON.parse(data);
+}
+module.exports ={
+    saveDb,
+    readDb
 }
