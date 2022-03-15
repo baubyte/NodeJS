f1ca9ae33ffeffc66acef4e46bff55cdeda33535
baubyte <paredbaez.martin@gmail.com>
2022-03-15 15:35:41 -0300

    serviendo archivos estaticos

--- 06_WebServer/app.js
+++ 06_WebServer/app.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const app = express()
+const port = 3000
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+//Middleware Contenido Estáticos
+app.use(express.static('public'));
+
+app.get('/hello-word', (req, res) => {
+    res.send('Hello World')
 })
+/**
+ * Cualquier otra ruta
+ */
+app.get('*', (req, res) => {
+    res.sendFile(__dirname+'/public/404.html');
+});
 
-app.listen(3000)
+app.listen(port, () => {
+    console.log(`Escuchando en el puerto ${port}`)
+  })
+  
