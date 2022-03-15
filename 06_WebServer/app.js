429ced1965f2283a618276a203b5cc5216329b08
baubyte <paredbaez.martin@gmail.com>
2022-03-15 13:27:37 -0300

    ejemplo webserver

--- 06_WebServer/app.js
+++ 06_WebServer/app.js
@@ -1,1 +1,23 @@
-console.log("Hola Mundo");
+const http = require('http');
+
+/**
+ * Creamos el servidor
+ */
+http.createServer((req, res)=>{
+
+    res.setHeader('Content-Disposition', 'attachment; filename=list.csv');
+    res.writeHead(200,{'Content-Type': 'application/json'});
+    //res.writeHead(200,{'Content-Type': 'application/csv'});
+    /**
+     * Ejemplo JSON
+     */
+    const persona = {
+        id: 1,
+        name:'BAUBYTE'
+    }
+
+    res.write(JSON.stringify(persona));
+    res.end();
+}).listen(8080);
+
+console.log("Listen Port. 8000");
