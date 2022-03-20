645daf8315a366f0344b877128f3e86843cb4810
baubyte <paredbaez.martin@gmail.com>
2022-03-20 19:02:51 -0300

    UserSchema Mongoose

---
+++ 07_RestServer/models/user.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+const { Schema, model } = mongoose;
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'El Nombre es Obligatorio.']
+    },
+    email: {
+        type: String,
+        required: [true, 'El Email es Obligatorio.'],
+        unique:true
+    },
+    password: {
+        type: String,
+        required: [true, 'La Contraseña es Obligatoria.']
+    },
+    image: {
+        type: String,
+    },
+    role: {
+        type: String,
+        required: [true, 'La Contraseña es Obligatoria.'],
+        enum: ['admin_role','user_role']
+    },
+    create_at: { type: Date, default: Date.now },
+    status: {
+        type: Boolean,
+        default: true
+    },
+    google: {
+        type: Boolean,
+        default: false
+    }
+});
+module.exports = model('User',UserSchema);
